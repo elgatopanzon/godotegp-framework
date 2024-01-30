6b3e3e8e2464a927fb5f8a8c1750407ce8d57e94
ElGatoPanzon <gato@elgatopanzon.io>
2024-01-30 15:57:31 -0600

    OpenAI: GatoGPT Extended: add ErrorResultExtended as an OpenAI compatible Error with additional Exception object

---
+++ classes/AI/OpenAI/ErrorResultExtended.cs
@@ -0,0 +1,24 @@
+/**
+ * @author      : ElGatoPanzon (contact@elgatopanzon.io) Copyright (c) ElGatoPanzon
+ * @file        : ErrorResultExtended
+ * @created     : Tuesday Jan 30, 2024 15:55:29 CST
+ */
+
+namespace GodotEGP.AI.OpenAI;
+
+using Godot;
+using GodotEGP.Objects.Extensions;
+using GodotEGP.Logging;
+using GodotEGP.Service;
+using GodotEGP.Event.Events;
+using GodotEGP.Config;
+
+public partial class ErrorResultExtended : BaseResult
+{
+	public ErrorResultExtendedObj Error { get; set; }
+}
+
+public partial class ErrorResultExtendedObj : ErrorResultObj
+{
+	public Exception Exception { get; set; }
+}
