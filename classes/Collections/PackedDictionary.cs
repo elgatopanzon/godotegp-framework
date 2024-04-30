16cb801644e6f5481eadc2c9ef737001cc5ba1c7
ElGatoPanzon <gato@elgatopanzon.io>
2024-04-29 18:52:37 -0600

    PackedDictionary: add PackedDictionaryBuckets for testing

--- classes/Collections/PackedDictionary.cs
+++ classes/Collections/PackedDictionary.cs
@@ -13,7 +13,9 @@ using GodotEGP.Service;
 using GodotEGP.Event.Events;
 using GodotEGP.Config;
 
+using System;
 using System.Collections.Generic;
+using System.Linq;
 
 public partial class PackedDictionary<TKey, TValue>
 {
@@ -120,6 +122,147 @@ public partial class PackedDictionary<TKey, TValue>
 		}
 
 		return false;
+	}
+}
+
+public partial class PackedDictionaryBuckets<TKey, TValue>
+{
+	// max size this dictionary can be (no growing)
+	private int _maxSize;
+
+	// size of buckets
+	private int _bucketSize;
+
+	// current size of the dictionary
+	private PackedArray<int> _currentSize;
+
+	// data stores for keys and values
+	private PackedArray<PackedArray<TKey>> _keys;
+	private PackedArray<PackedArray<TValue>> _values;
+
+	// expose Keys and Values properties
+	internal TKey[] Keys {
+		get {
+			return JoinBuckets<TKey>(_keys);
+		}
+	}
+	internal TValue[] Values {
+		get {
+			return JoinBuckets<TValue>(_values);
+		}
+	}
+
+	// allow accessing indexes like regular dictionary
+	public TValue this[TKey key] {
+		get {
+			return Get(key);
+		}
+		set {
+			Insert(key, value);
+		}
+	}
+
+	public PackedDictionaryBuckets(int maxSize = 0, int bucketSize = 10)
+	{
+		_maxSize = maxSize;
+		_bucketSize = bucketSize;
+
+		_keys = new();
+		_values = new();
+		_currentSize = new();
+
+		// init buckets and bucket sizes
+		for (int i = 0; i < bucketSize; i++)
+		{
+			_keys[i] = new();
+			_values[i] = new();
+			_currentSize[i] = 0;
+		}
+	}
+
+	public T[] JoinBuckets<T>(PackedArray<PackedArray<T>> buckets)
+	{
+		PackedArray<T> joined = new PackedArray<T>(buckets.Array.Sum(a => a.Count));
+		int position = 0;
+		foreach (var a in buckets)
+		{
+			Array.Copy(a.Array, 0, joined.Array, position, a.Count);
+			position += a.Count;
+		}
+
+		return joined.Array;
+	}
+
+	public TValue Get(TKey key)
+	{
+		int bucket = GetBucket(key);
+
+		return _values[bucket][GetIndex(key, bucket)];
+	}
+	public ref TValue GetRef(TKey key)
+	{
+		int bucket = GetBucket(key);
+		int index = GetIndex(key, bucket);
+
+		return ref _values[bucket].GetRef(GetIndex(key, bucket));
+	}
+
+	public int GetIndex(TKey key, int bucket)
+	{
+		return System.Array.IndexOf(_keys[bucket].Array, key);
+	}
+
+	public int GetBucket(TKey key)
+	{
+		uint hashCode = (uint)key.GetHashCode();
+		uint bucketCount = (uint) _bucketSize;
+		uint maxSize = 10;
+		ulong multiplier = ulong.MaxValue / maxSize + 1;
+        uint highbits = (uint)(((((multiplier * hashCode) >> 32) + 1) * bucketCount) >> 32);
+
+        int bucket = (int) highbits;
+
+        return bucket;
+	}
+
+	public void Add(TKey key, TValue value)
+	{
+		Insert(key, value);
+	}
+
+	public void Insert(TKey key, TValue value)
+	{
+		int bucket = GetBucket(key);
+		int keyIndex = GetIndex(key, bucket);
+
+		if (keyIndex == -1)
+		{
+			keyIndex = _currentSize[bucket];
+			_currentSize[bucket]++;
+		}
+
+		// add the key and value to the backing arrays
+		_keys[bucket].Insert(keyIndex, key);
+		_values[bucket].Insert(keyIndex, value);
+	}
+
+	public void Remove(TKey key)
+	{
+		int bucket = GetBucket(key);
+		int keyIndex = GetIndex(key, bucket);
+
+		// remove the key and value from the backing store
+		_keys[bucket].RemoveAt(keyIndex);
+		_values[bucket].RemoveAt(keyIndex);
+
+		_currentSize[bucket]--;
+	}
+
+	public bool ContainsKey(TKey key)
+	{
+		int bucket = GetBucket(key);
+		
+		return (GetIndex(key, bucket) != -1);
 	}
 }
 
