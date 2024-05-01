3c3e4baf45d5f6573785f2473b1bc4743e37fea0
ElGatoPanzon <gato@elgatopanzon.io>
2024-04-30 19:54:00 -0600

    PackedArray: fix Contains() and Remove() by implementing a working IndexOf() that respects the managed size

--- classes/Collections/PackedArray.cs
+++ classes/Collections/PackedArray.cs
@@ -46,8 +46,8 @@ public partial class PackedArray<T> : IEnumerable, IEnumerator
 	}
 
 	// array of index mappings to underlying array indexes
-	private int[] _indexToDataMap;
 	private int[] _dataToIndexMap;
+	private int[] _indexToDataMap;
 
 	// allow accessing indexes like regular array
 	public T this[int index] {
@@ -66,43 +66,51 @@ public partial class PackedArray<T> : IEnumerable, IEnumerator
 
 		// init the data array, and the map arrays
 		_array = new T[maxSize];
-		_indexToDataMap = new int[maxSize];
 		_dataToIndexMap = new int[maxSize];
+		_indexToDataMap = new int[maxSize];
+
+		LoggerManager.LogDebug("Init packed array", typeof(T).Name, "size", maxSize);
 
 		ClearDataIndexes();
 	}
 
 	public void ClearDataIndexes(int startFrom = 0)
 	{
+		LoggerManager.LogDebug("Clearing data indexes", typeof(T).Name);
+
 		for (int i = startFrom; i < _maxSize; i++)
 		{
-			_indexToDataMap[i] = -1;
 			_dataToIndexMap[i] = -1;
+			_indexToDataMap[i] = -1;
 		}
 	}
 
 	public T Get(int index)
 	{
-		int dataIndex = _indexToDataMap[index];
+		int dataIndex = _dataToIndexMap[index];
 		return _array[dataIndex];
 	}
 	public ref T GetRef(int index)
 	{
-		int dataIndex = _indexToDataMap[index];
+		int dataIndex = _dataToIndexMap[index];
 		return ref _array[dataIndex];
 	}
 
 	// add value and grow array size
 	public void Add(T value)
 	{
+		LoggerManager.LogDebug("Appending value", typeof(T).Name, _currentSize.ToString(), value);
+
 		Insert(_currentSize, value);
 	}
 
 	public void Resize(int newSize)
 	{
 		System.Array.Resize<T>(ref _array, newSize);
-		System.Array.Resize<int>(ref _indexToDataMap, newSize);
 		System.Array.Resize<int>(ref _dataToIndexMap, newSize);
+		System.Array.Resize<int>(ref _indexToDataMap, newSize);
+
+		LoggerManager.LogDebug("Resizing array", typeof(T).Name, "size", $"{_maxSize} => {newSize}");
 
 		_maxSize = newSize;
 	}
@@ -116,55 +124,114 @@ public partial class PackedArray<T> : IEnumerable, IEnumerator
 			ClearDataIndexes(_currentSize);
 		}
 
+		LoggerManager.LogDebug("Inserting value", typeof(T).Name, index.ToString(), value);
+
 		// add data to the end of the array at current size
 		_array[_currentSize] = value;
 
 		// add entries to index maps
-		_indexToDataMap[index] = _currentSize;
-		_dataToIndexMap[_currentSize] = index;
+		_dataToIndexMap[index] = _currentSize;
+		_indexToDataMap[_currentSize] = index;
+
+		LoggerManager.LogDebug("_dataToIndexMap", typeof(T).Name, "_dataToIndexMap", _indexToDataMap);
+		LoggerManager.LogDebug("_indexToDataMap", typeof(T).Name, "_indexToDataMap", _dataToIndexMap);
 		
 		// increment current size
 		_currentSize++;
+
+		LoggerManager.LogDebug("_array", typeof(T).Name, "_array", _array);
 	}
 
 	// remove and keep array packed
 	public void RemoveAt(int index)
 	{
+		LoggerManager.LogDebug("Removing index", typeof(T).Name, "index", index);
+
 		// move the end element to the deleted element's position
 		int lastElementIndex = _currentSize - 1;
-		_array[_dataToIndexMap[index]] = _array[lastElementIndex];
+		int removedElementIndex = _dataToIndexMap[index];
+
+		LoggerManager.LogDebug("Last element index", typeof(T).Name, "lastElementIndex", lastElementIndex);
+		LoggerManager.LogDebug("_array", typeof(T).Name, "_array", _array);
+		LoggerManager.LogDebug("_dataToIndexMap", typeof(T).Name, "_dataToIndexMap", _indexToDataMap);
+		LoggerManager.LogDebug("_indexToDataMap", typeof(T).Name, "_indexToDataMap", _dataToIndexMap);
+		LoggerManager.LogDebug("_dataToIndexMap[index]", typeof(T).Name, "_dataToIndexMap[index]", _indexToDataMap[index]);
+		LoggerManager.LogDebug("_array[lastElementIndex]", typeof(T).Name, "_array[lastElementIndex]", _array[lastElementIndex]);
+
+		_array[removedElementIndex] = _array[lastElementIndex];
 
 		// update the map of indexes to reflect the change
-		_indexToDataMap[_dataToIndexMap[lastElementIndex]] = _dataToIndexMap[index];
-		_dataToIndexMap[_dataToIndexMap[index]] = _dataToIndexMap[lastElementIndex];
+		int indexOfLastElement = _indexToDataMap[lastElementIndex];
+		_dataToIndexMap[indexOfLastElement] = removedElementIndex;
+		_indexToDataMap[removedElementIndex] = indexOfLastElement;
 
 		// invalidate the old index mappings
-		_indexToDataMap[index] = -1;
-		_dataToIndexMap[lastElementIndex] = -1;
+		_dataToIndexMap[index] = -1;
+		_indexToDataMap[lastElementIndex] = -1;
+
+		// _array[_dataToIndexMap[index]] = _array[lastElementIndex];
+        //
+		// // update the map of indexes to reflect the change
+		// _indexToDataMap[_dataToIndexMap[lastElementIndex]] = _dataToIndexMap[index];
+		// _dataToIndexMap[_dataToIndexMap[index]] = _dataToIndexMap[lastElementIndex];
+        //
+		// // invalidate the old index mappings
+		// _indexToDataMap[index] = -1;
+		// _dataToIndexMap[lastElementIndex] = -1;
 
 		// decrease array size
 		_currentSize--;
+
+		LoggerManager.LogDebug("_dataToIndexMap", typeof(T).Name, "_dataToIndexMap", _indexToDataMap);
+		LoggerManager.LogDebug("_indexToDataMap", typeof(T).Name, "_indexToDataMap", _dataToIndexMap);
+
+		LoggerManager.LogDebug("_array", typeof(T).Name, "_array", _array);
+		LoggerManager.LogDebug("_indexToDataMap", typeof(T).Name, "_indexToDataMap", _dataToIndexMap);
 	}
 
 	public void Remove(T value)
 	{
-		for (int i = 0; i < _maxSize; i++)
+		LoggerManager.LogDebug("Removing value", typeof(T).Name, "value", value);
+
+		int index = IndexOf(value);
+
+		if (index != -1)
 		{
-			if (_array[i].Equals(value))
-			{
-				RemoveAt(_dataToIndexMap[i]);
-			}
+			LoggerManager.LogDebug("Found value at real index", typeof(T).Name, "index", index);
+			LoggerManager.LogDebug("Found value indexToData", typeof(T).Name, "indexToData", _indexToDataMap[index]);
+			LoggerManager.LogDebug("Found value dataToIndex", typeof(T).Name, "dataToIndex", _dataToIndexMap[index]);
+
+			LoggerManager.LogDebug("_dataToIndexMap", typeof(T).Name, "_dataToIndexMap", _indexToDataMap);
+			LoggerManager.LogDebug("_indexToDataMap", typeof(T).Name, "_indexToDataMap", _dataToIndexMap);
+
+			RemoveAt(_indexToDataMap[index]);
 		}
+
+		LoggerManager.LogDebug("_array", typeof(T).Name, "_array", _array);
+		LoggerManager.LogDebug("_indexToDataMap", typeof(T).Name, "_indexToDataMap", _dataToIndexMap);
 	}
 
 	public bool ContainsIndex(int index)
 	{
-		return _indexToDataMap[index] != -1;
+		return _dataToIndexMap[index] != -1;
 	}
 
 	public bool Contains(T value)
 	{
-		return (System.Array.IndexOf(_array, value) != -1);
+		return (IndexOf(value) != -1);
+	}
+
+	public int IndexOf(T value)
+	{
+		for (int i = 0; i < _currentSize; i++)
+		{
+			if (_array[i].Equals(value))
+			{
+				return i;
+			}
+		}
+
+		return -1;
 	}
 
 	IEnumerator IEnumerable.GetEnumerator()
@@ -212,7 +279,7 @@ public partial class PackedArray<T> : IEnumerable, IEnumerator
         		{
         			SetOrderedIndexMap();
         		}
-                return _array[_indexToDataMap[_enumeratorOrderedIndexMap[_enumeratorPosition]]];
+                return _array[_dataToIndexMap[_enumeratorOrderedIndexMap[_enumeratorPosition]]];
             }
             catch (IndexOutOfRangeException)
             {
@@ -223,14 +290,14 @@ public partial class PackedArray<T> : IEnumerable, IEnumerator
 
     public void SetOrderedIndexMap()
     {
-        _enumeratorOrderedIndexMap = _dataToIndexMap.OrderBy(x => x).ToArray();
+        _enumeratorOrderedIndexMap = _indexToDataMap.OrderBy(x => x).ToArray();
     }
 
     public IEnumerable<T> OrderedArray
     {
 		get {
-			return _dataToIndexMap.Where((x) => x >= 0).OrderBy(x => x).Select<int, T>((x) => {
-				return _array[_indexToDataMap[x]];
+			return _indexToDataMap.Where((x) => x >= 0).OrderBy(x => x).Select<int, T>((x) => {
+				return _array[_dataToIndexMap[x]];
 			});
 		}
     }
@@ -270,6 +337,9 @@ public partial class PackedArrayDictBacked<T> : IEnumerable, IEnumerator
 	public T this[int index] {
 		get {
 			return Get(index);
+		}
+		set {
+			Insert(index, value);
 		}
 	}
 
@@ -426,6 +496,9 @@ public partial class PackedArrayDictionary<T> : IEnumerable, IEnumerator
 	public T this[int index] {
 		get {
 			return Get(index);
+		}
+		set {
+			Insert(index, value);
 		}
 	}
 
